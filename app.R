3ade591b4ad691b8fc5f6e103838b288f781d838
kostas.pateras@gmail.com <63882794+kpateras@users.noreply.github.com>
2023-06-30 17:33:40 +0300

    Brief update (Kostas)
    Updated sidebar interface, added icons
    Updated plot and plot abilities,
    Added option for table output, selecting a table and for downloading.

--- app.R
+++ app.R
@@ -6,8 +6,15 @@
 
 library(shiny)    
 library(shinydashboard)
+library(shinyWidgets)
+library(plotly)
+library(DT)
 # library(EVI)
 library(ggplot2)
+
+load("EVI_Global")
+load("cEVI_Global")
+#load("cEVI_Africa")
 
 ui <- dashboardPage(
   
@@ -23,80 +30,102 @@ ui <- dashboardPage(
   
   # SIDEBAR
   dashboardSidebar(
-    width = 300,
+    width = 470,
     div(class = "inlay", style = "height:15px;width:100%;background-color: #ecf0f5;"),
     
     sidebarUserPanel("Version 1.0"),
-    
-    sidebarMenu(id ="tabs",
-      menuItem("Time Period", tabName = "Time_Period", icon = icon("bar-chart")),
-              dateRangeInput("rdates_Global", "Range of dates for Avian influenza outbreaks:", start =as.Date(EVI_Global$Days, origin="1970-01-01")[nrow(EVI_Global)-365], end = max(as.Date(EVI_Global$Days, origin="1970-01-01"))), # input$rdates_Global
-              
-              
-      
-            
-       #menuItem("ReadMe", tabName = "readme", icon=icon("mortar-board")),
-      #menuItem("About", tabName = "about", icon = icon("question")),
-      hr(),
-      menuItem("Plot Features", tabName = "dashboard", icon = icon("line-chart"),
-               
-               checkboxInput("rlines", "Draw lines instead of points (EVI plots)", value = FALSE), # input$rlines==FALSE
-               checkboxInput("rlog", "Logarithmic scale", value = FALSE)
-               
-               #      numericInput("rsize", "Size of points", value = 1.5) # input$rsize
-               
-              )
-      
-               
-      
-
-      
-      
-    ),
-    
-    hr(),
+    tabsetPanel(
+      tabPanel(
+        "Home",
+        icon = icon("home"),
+        sidebarMenu(
+          id = "tabs",
+          menuItem("Time Period", tabName = "Time_Period", icon = icon("bar-chart")),
+          dateRangeInput(
+            "rdates_Global",
+            "Range of dates for Avian influenza outbreaks:",
+            start = as.Date(EVI_Global$Days, origin = "1970-01-01")[nrow(EVI_Global) - 365],
+            end = max(as.Date(EVI_Global$Days, origin = "1970-01-01"))
+          ), # input$rdates_Global
+          
+          hr(),
+          menuItem(
+            "Plot Features",
+            tabName = "dashboard",
+            icon = icon("line-chart"),
+            checkboxInput(
+              "rlines",
+              "Draw lines instead of points (EVI plots)",
+              value = FALSE
+            ), # input$rlines==FALSE
+            checkboxInput("rlog", "Logarithmic scale", value = FALSE)
+            # numericInput("rsize", "Size of points", value = 1.5) # input$rsize
+          ),
+          
+          menuItem(
+            "Table Features",
+            tabName = "dashboard2",
+            icon = icon("table"),
+            switchInput(
+              inputId = "evi_cevi", label = "- Mode -",
+              onLabel = "EVI", offLabel = "cEVI", value = TRUE,
+              width = "100%"
+            )
+        )
+        )
+        ),
+      tabPanel(
+        "Custom",
+        icon = icon("pen"),
+        "Here we will place options for customizing figures",
+        sliderInput(inputId = "sizeindex",label = "Point size",min = 0.5,max = 5,value = 1,step = 0.5),
+      ),
+    tabPanel(
+      "Ackws",
+      icon = icon("user"),
     h4("References"),
-    helpText(tags$b(""),"Kostoulas, P., Meletis, E., Pateras, K. et al. The epidemic volatility index, a novel early warning tool for identifying new waves in an epidemic. Sci Rep 11, 23775 (2021)",
-             tags$a(href="https://www.nature.com/articles/s41598-021-02622-3", 
-                    "Read it here")),
-    helpText(tags$b(""),"Meletis E. et al. EVI R-package: Epidemic Volatility Index as an Early-Warning Tool",
-             tags$a(href="https://cran.r-project.org/web/packages/EVI/EVI.pdf", 
-                    "R-package Info")),
-    helpText(tags$b(""),"Pateras K., et al. The convergence epidemic index (cEVI) an early warning tool for identifying waves in an epidemic.",
-             tags$a(href="https://www.sciencedirect.com/science/article/pii/S2468042723000349", 
-                    "Read it here")),
-    
-    
-    tabPanel("Info", tabName = "Widgets", icon = icon("th"),
-             
-             h4("Figures Explained"),
-             h5("Upper left:"),
-             h6("Observations (updated daily), presented on the original scale, with red dots corresponding to dates that, according to EVI, an early warning was issued."),
-             h5("Upper right:"),
-             h6("Observations (updated daily), presented on the logarithmic scale, which facilitates the comparison of the steepness of the epidemic curve between the different waves."),
-             hr(),
-             h5("Model Predictive value"),
-             h6("Positive predictive value (PPV) for the days that an early warning was issued. Higher color intensity corresponds to PPV closer to the value of 1."),
-             h6("Negative predictive values (NPV) for the days that an early warning was not issued. Higher color intensity corresponds to NPV closer to the value of 1.")
-             
+    helpText(tags$b(""), "Kostoulas, P., Meletis, E., Pateras, K. et al. The epidemic volatility index, a novel early warning tool for identifying new waves in an epidemic. Sci Rep 11, 23775 (2021)",
+             tags$a(
+               href = "https://www.nature.com/articles/s41598-021-02622-3",
+               "Read it here"
+             )
+    ),
+    helpText(tags$b(""), "Meletis E. et al. EVI R-package: Epidemic Volatility Index as an Early-Warning Tool",
+             tags$a(
+               href = "https://cran.r-project.org/web/packages/EVI/EVI.pdf",
+               "R-package Info"
+             )
+    ),
+    helpText(tags$b(""), "Pateras K., et al. The convergence epidemic index (cEVI) an early warning tool for identifying waves in an epidemic.",
+             tags$a(
+               href = "https://www.sciencedirect.com/science/article/pii/S2468042723000349",
+               "Read it here"
+             )
+    ),
+    ),
+    tabPanel(
+      "Info",
+      icon = icon("info"),
+      h4("Figures Explained"),
+      h5("Upper left:"),
+      h6("Observations (updated daily), presented on the original scale, with red dots corresponding to dates that, according to EVI, an early warning was issued."),
+      h5("Upper right:"),
+      h6("Observations (updated daily), presented on the logarithmic scale, which facilitates the comparison of the steepness of the epidemic curve between the different waves."),
+      hr(),
+      h5("Model Predictive value"),
+      h6("Positive predictive value (PPV) for the days that an early warning was issued. Higher color intensity corresponds to PPV closer to the value of 1."),
+      h6("Negative predictive values (NPV) for the days that an early warning was not issued. Higher color intensity corresponds to NPV closer to the value of 1.")
     )
+  )
   ),
   
   
   dashboardBody(
-    
-    tabBox(width=NULL,
-    
-      tabPanel(h5("Avian Influenza"),
-               fluidRow(
-                 box(plotOutput("boxGlobal1"))#,
-                 #box(plotOutput("boxGlobal2"))
-               )
-      )
-
-  
-
-
+    tabPanel(h5("Avian Influenza"),
+             fluidRow(
+               plotlyOutput("boxGlobal1", width = "100%"),
+               dataTableOutput("EVI_cEVI_Global")
+             )
     )
   )
 )
@@ -107,7 +136,7 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
   
-evirlap <- function(Index1,Index2, ln=T, type="p",size.index=1,
+  evirlap <- function(Index1,Index2, ln=T, type="p",size.index=1,
                       Index1.lab="EVI1",Index2.lab="EVI2",Index3.lab="EVI-",Index.country=NULL, origin="1970-01-01") {
     
     Index1$Days = as.Date(Index1$Days, origin="1970-01-01")
@@ -164,24 +193,48 @@ evirlap <- function(Index1,Index2, ln=T, type="p",size.index=1,
     print(sp3)
     
   }
-  output$boxGlobal1 <- renderPlot({
-      par(mfrow=c(1,1))
-      LL=ifelse(identical(which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1]),integer(0)), yes = which(as.Date(EVI_Global$Days, origin="1970-01-01")==(as.Date(input$rdates_Global[1]+1))), no = which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1])) 
-      UL=ifelse(identical(which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[2]),integer(0)), yes = which(as.Date(EVI_Global$Days, origin="1970-01-01")==(as.Date(input$rdates_Global[2]-1))), no = which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[2])) 
-      evirlap(Index1 = EVI_Global[LL:UL,], Index2 = cEVI_Global[LL:UL,],
-              Index1.lab = "EVI", Index2.lab = "cEVI", Index3.lab = "EVI-", ln = ifelse(test=input$rlog, T , F), Index.country = "HPAI", type = ifelse(test = input$rlines,"l","p"))
-      
+  output$boxGlobal1 <- renderPlotly({
+    options()
+    par(mfrow=c(1,1))
+    LL=ifelse(identical(which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1]),integer(0)), yes = which(as.Date(EVI_Global$Days, origin="1970-01-01")==(as.Date(input$rdates_Global[1]+1))), no = which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1])) 
+    UL=ifelse(identical(which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[2]),integer(0)), yes = which(as.Date(EVI_Global$Days, origin="1970-01-01")==(as.Date(input$rdates_Global[2]-1))), no = which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[2])) 
+    evirlap(Index1 = EVI_Global[LL:UL,], Index2 = cEVI_Global[LL:UL,],size.index = input$sizeindex,
+            Index1.lab = "EVI", Index2.lab = "cEVI", Index3.lab = "cEVI-", ln = ifelse(test=input$rlog, T , F), Index.country = "HPAI", type = ifelse(test = input$rlines,"l","p"))
+    
   })
   
-#  output$boxGlobal2 <- renderPlot({
-#    LL=ifelse(identical(which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1]),integer(0)), yes = which(as.Date(EVI_Global$Days, origin="1970-01-01")==(as.Date(input$rdates_Global[1]+1))), no = which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1])) 
-#    UL=ifelse(identical(which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1]),integer(0)), yes = which(as.Date(EVI_Global$Days, origin="1970-01-01")==(as.Date(input$rdates_Global[2]-1))), no = which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[2])) 
-#    evirlap(Index1 = EVI_Global, Index2 = cEVI_Global,
-#            Index1.lab = "EVI", Index2.lab = "cEVI", Index3.lab = "EVI-", ln = T, Index.country = "HPAI", type = ifelse(test = input$rlines,"l","p"))
-#  })
+  #  output$boxGlobal2 <- renderPlot({
+  #    LL=ifelse(identical(which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1]),integer(0)), yes = which(as.Date(EVI_Global$Days, origin="1970-01-01")==(as.Date(input$rdates_Global[1]+1))), no = which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1])) 
+  #    UL=ifelse(identical(which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[1]),integer(0)), yes = which(as.Date(EVI_Global$Days, origin="1970-01-01")==(as.Date(input$rdates_Global[2]-1))), no = which(as.Date(EVI_Global$Days, origin="1970-01-01")==input$rdates_Global[2])) 
+  #    evirlap(Index1 = EVI_Global, Index2 = cEVI_Global,
+  #            Index1.lab = "EVI", Index2.lab = "cEVI", Index3.lab = "EVI-", ln = T, Index.country = "HPAI", type = ifelse(test = input$rlines,"l","p"))
+  #  })
   
+  EVI_cEVI_Global<-renderUI({
 
+  }) 
   
+  output$EVI_cEVI_Global <- DT::renderDataTable({
+    
+    if(input$evi_cevi==TRUE) {out<-EVI_Global}
+    if(input$evi_cevi==FALSE) {out<-cEVI_Global;names(out)[2]<-"cEVI"}
+    #out<-EVI_Global
+    datatable(
+      out,
+      extensions = 'Buttons',
+      options = list(
+        paging = TRUE,
+        searching = TRUE,
+        fixedColumns = TRUE,
+        autoWidth = TRUE,
+        ordering = TRUE,
+        dom = 'Blfrtip',
+        buttons = c('copy', 'csv', 'excel'),
+        lengthMenu = list(c(10, 30, 50, -1),
+                          c('10', '30', '50', 'All')),
+        paging = T)
+    )
+  })
   # as.Date(EVI_Global$Days, origin="2024-12-31")
   
 }
